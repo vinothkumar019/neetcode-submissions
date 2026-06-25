33e4f91bba67a965079cb348d5628eea8f095d02
Vinothkumar R <vinothkumarr019@gmail.com>
2026-06-25 14:32:55 +0000

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+
+        sort(intervals.begin(), intervals.end(),
+             [](Interval &a, Interval &b){
+                 return a.start < b.start;
+             });
+
+        for(int i = 1; i < intervals.size(); i++){
+
+            if(intervals[i].start < intervals[i-1].end)
+                return false;
+        }
+
+        return true;
+    }
+};
