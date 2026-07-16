fd5dbef520257e0bfa5532ac415e4739248bc1a7
Vinothkumar R <vinothkumarr019@gmail.com>
2026-07-16 23:42:40 +0530

    Add: matchsticks-to-square - submission-3

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool dfs(vector<int>& matchsticks,int index,int target,vector<int> side){
+        if(index == matchsticks.size()) return true;
+
+        for(int i=0;i<4;i++){
+            if(side[i]+matchsticks[index] <= target){
+            side[i] += matchsticks[index];
+             if(dfs(matchsticks,index+1,target,side))
+                return true;
+            side[i] -=  matchsticks[index];
+            }
+            if(side[i] == 0) break;
+        }
+        return false;
+    }
+    bool makesquare(vector<int>& matchsticks) {
+        int sum=0;
+        int target=0;
+        for(int i:matchsticks){
+            sum += i;
+        }
+        if(sum%4 != 0) return false;
+        sort(matchsticks.rbegin(), matchsticks.rend());
+        target=sum/4;
+        vector<int> side(4,0);
+
+        return dfs(matchsticks,0,target,side);
+    }
+};
