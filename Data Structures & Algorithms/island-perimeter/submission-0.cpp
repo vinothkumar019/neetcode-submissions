a7156d7b677b5c8fcf21d181d2f87f4f1832f1d6
Vinothkumar R <vinothkumarr019@gmail.com>
2026-07-17 23:58:34 +0530

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int perimeter = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+
+                    // Shared edge with right cell
+                    if (j + 1 < n && grid[i][j + 1] == 1)
+                        perimeter -= 2;
+
+                    // Shared edge with bottom cell
+                    if (i + 1 < m && grid[i + 1][j] == 1)
+                        perimeter -= 2;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+};
